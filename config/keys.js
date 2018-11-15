ed2fc7f7a3cb6797f372c6c3e8bb1d893a132fec
Roman Yablonski <ryablonski@elilink.com>
2018-11-15 19:29:32 +0300

    added login function

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://roman.yablonski:Romeosun1@ds135653.mlab.com:35653/my-blog' // кладем сюда только для того, чтобы было боле аккуратно
+    mongoURI: 'mongodb://roman.yablonski:Romeosun1@ds135653.mlab.com:35653/my-blog', // кладем сюда только для того, чтобы было боле аккуратно
+    jwt: 'dev-jwt'
 }
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,40 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const keys = require('../config/keys');
 
-module.exports.login = function(req, res) {
-    res.status(200).json({
-        login: {
-            email: req.body.email,
-            password: req.body.password
+module.exports.login = async function(req, res) {
+    const candidate = await User.findOne({email: req.body.email});
+
+    if (candidate) {
+        // Проверка пользователя
+        const passwordResult = bcrypt.compareSync(req.body.password, candidate.password);
+        if (passwordResult) {
+            // Генерация токена, пароли совпали
+            const token = jwt.sign({
+                email: candidate.email,
+                userId: candidate._id
+            }, keys.jwt, {expiresIn: 3600});
+
+            res.status(200).json({
+                token: `Bearer ${token}`
+            })
+        } else {
+            // Пароли не совпали
+            res.status(401).json({
+                message: 'Incorrect password'
+            })
         }
-    })
+    } else {
+        // Пользователя нет, ошибка
+        res.status(404).json({
+            message: 'User not found'
+        })
+    }
 };
 
 module.exports.register = async function(req, res) {
-    const candidate = await User.findOne({email: req.body.email})
+    const candidate = await User.findOne({email: req.body.email});
 
     if(candidate) {
         // Пользователь существует, отдаем ошибку
